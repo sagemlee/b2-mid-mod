fe63aa15d93c988a75ee5045410081677729afce
Sage Lee <sagemcklee@gmail.com>
2020-05-22 11:41:08 -0600

    Add more robust model testing

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Actor do
   describe "relationships" do
+    it { should have_many(:movie_actors)}
     it { should have_many(:movies).through(:movie_actors)}
   end
   describe "methods" do
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
-  describe "validations" do
+  describe "relationships" do
+    it { should have_many(:movie_actors)}
+    it { should have_many(:actors).through(:movie_actors)}
     it {should belong_to :studio}
   end
+
 end
