e85e178d5785631739a5750dc271b51a9883b132
Sage Lee <sagemcklee@gmail.com>
2020-05-22 09:50:20 -0600

    Revert "Add studio index view, method in the controller, passing rspec user story1"
    This reverts commit 5df64f361403bace5941649f504db4c2ec6f2538.

--- db/migrate/20200522152214_create_studios.rb
+++
@@ -1,9 +0,0 @@
-class CreateStudios < ActiveRecord::Migration[5.1]
-  def change
-    create_table :studios do |t|
-      t.string :name
-      t.string :location
-      t.timestamps
-    end
-  end
-end
--- spec/studios/studio_index_spec.rb
+++ spec/studios/studio_index_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "Studio index page", type: :feature  do
+RSpec.describe "Studio index page" do
   it "should display all movie studios, each studio should also have a list of its movies" do
     disney = Studio.create!(name: "Disney", location: "Los Angeles")
     warnerbros = Studio.create!(name: "Warner Brothers", location: "Southern California")
     pixar = Studio.create!(name: "Pixar", location: "Studio City")
 
-    movie1 = disney.movies.create!(title: "Movie 1", creation_year: 2011, genre: "horror")
-    movie2 = warnerbros.movies.create!(title: "Movie 2", creation_year: 1999, genre: "comedy")
-    movie3 = pixar.movies.create!(title: "Movie 3", creation_year: 2005, genre: "romance")
-    movie4 = pixar.movies.create!(title: "Movie 4", creation_year: 2020, genre: "family")
+    movie1 = disney.movie.create!(title: "Movie 1", creation_year: 2011, genre: "horror")
+    movie2 = warnerbros.movie.create!(title: "Movie 2", creation_year: 1999, genre: "comedy")
+    movie3 = pixar.movie.create!(title: "Movie 3", creation_year: 2005, genre: "romance")
+    movie4 = pixar.movie.create!(title: "Movie 4", creation_year: 2020, genre: "family")
 
 
     visit '/studios'
 
     within "#studio-#{disney.id}" do
-      expect(page).to have_content("#{movie1.title}")
+      expect(page).to have_content("#{movie1.name}")
     end
 
     within "#studio-#{warnerbros.id}" do
-      expect(page).to have_content("#{movie2.title}")
+      expect(page).to have_content("#{movie2.name}")
     end
 
     within "#studio-#{pixar.id}" do
-      expect(page).to have_content("#{movie3.title}")
-      expect(page).to have_content("#{movie4.title}")
+      expect(page).to have_content("#{movie3.name}")
+      expect(page).to have_content("#{movie4.name}")
     end
 
   end
